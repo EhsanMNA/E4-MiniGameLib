1238fda6a5dbf876b19d92f0b9a2d06ab0bed17b
EhsanMNA <0ehsanmna0@gmail.com>
2022-02-22 22:09:35 +0330

    Nms + pom.xml has been updated

--- src/main/java/ir/e4Team/e4minigamelib/Utilities/NMS/v1_17/NmsVersion_1_17.java
+++ src/main/java/ir/e4Team/e4minigamelib/Utilities/NMS/v1_17/NmsVersion_1_17.java
@@ -1,6 +1,15 @@
 package ir.e4Team.e4minigamelib.Utilities.NMS.v1_17;
 
 import ir.e4Team.e4minigamelib.Utilities.NMS.NmsUtils;
+import ir.e4Team.e4minigamelib.Utilities.Utils;
+import net.minecraft.network.chat.IChatBaseComponent;
+import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket;
+import net.minecraft.network.protocol.game.ClientboundSetTitleTextPacket;
+import net.minecraft.server.level.EntityPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
 import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
@@ -9,17 +18,21 @@ public class NmsVersion_1_17 implements NmsUtils {
     public void sendTitle(Player player, String message, int time) {
         CraftPlayer cp = (CraftPlayer) player;
         EntityPlayer p = cp.getHandle();
-        PacketPlayOutTitle packet = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.TITLE, IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + message + "\"}"),time,time,time);
-        p.playerConnection.sendPacket(packet);
+        ClientboundSetTitleTextPacket packet = new ClientboundSetTitleTextPacket(IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + message + "\"}"));
+        p.b.sendPacket(packet);
     }
 
     @Override
     public void sendActionBar(Player player, String message, int time) {
-
+        CraftPlayer cp = (CraftPlayer) player;
+        EntityPlayer p = cp.getHandle();
+        ClientboundSetActionBarTextPacket packet = new ClientboundSetActionBarTextPacket(IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + message + "\"}"));
+        p.b.sendPacket(packet);
     }
 
     @Override
     public void sendBossBar(Player player, String message, float health) {
-
+        BossBar bar = Bukkit.createBossBar(Utils.color(message), BarColor.GREEN, BarStyle.SOLID);
+        bar.addPlayer(player);
     }
 }
